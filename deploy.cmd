9bcb6647fe4b5464c3f8439bfb28ab65f9edeb60
Rasmi Elasmar <rrelasmar@gmail.com>
2015-06-23 17:06:38 -0400

    Attempting to add DATE to azure script.

--- deploy.cmd
+++ deploy.cmd
@@ -88,6 +88,7 @@ echo Handling python deployment.
 @echo off
 for /F "usebackq tokens=1,2 delims==" %%i in (`wmic os get LocalDateTime /VALUE 2^>NUL`) do if '.%%i.'=='.LocalDateTime.' set ldt=%%j
 set ldt=%ldt:~0,4%-%ldt:~4,2%-%ldt:~6,2% %ldt:~8,2%:%ldt:~10,2%:%ldt:~12,6%
+echo %ldt%
 echo Backing up current database.
 copy /y "%DEPLOYMENT_TARGET%\civic.db" "%DEPLOYMENT_TARGET%\civic.db.BACKUP-%ldt%"
 copy /y "%DEPLOYMENT_TARGET%\civic.db" "%DEPLOYMENT_TARGET%\civic.db.BACKUP"
