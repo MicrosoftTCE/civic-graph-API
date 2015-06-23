2bc59fcf541dbac4d2a24e4141ca361cb78f03c8
Rasmi Elasmar <rrelasmar@gmail.com>
2015-06-23 14:47:48 -0400

    Added location error handling and logging.

--- __init__.py
+++ __init__.py
@@ -1,4 +1,7 @@
 from app import app
 
 if __name__ == '__main__':
+    if not app.debug:
+        import logging
+        logging.basicConfig(filename='error.log',level=logging.DEBUG)
     app.run()
