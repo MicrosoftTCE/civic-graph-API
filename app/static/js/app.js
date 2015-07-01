cb4f636fce0f6cae6e284fe6ea7b21d81536f807
David Lorente <david@Davids-MacBook-Pro.local>
2015-07-01 11:14:26 -0400

    merged

--- app/static/js/app.js
+++ app/static/js/app.js
@@ -22,7 +22,7 @@ angular.module('civic-graph', ['ui.bootstrap', 'leaflet-directive'])
     window.mobilecheck = function() {
         var check = false;
         (function(a){if(/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows ce|xda|xiino/i.test(a)||/1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(a.substr(0,4)))check = true})(navigator.userAgent||navigator.vendor||window.opera);
-        return check;
+        return true;
     }
     $scope.mobile = window.mobilecheck();
     $scope.settingsEnabled = !$scope.mobile;
@@ -376,14 +376,14 @@ angular.module('civic-graph', ['ui.bootstrap', 'leaflet-directive'])
             links: _.flatten(_.values($scope.connections))
         }
         var colors = {
-            'Government': 'rgb(242, 80, 34)',
-            'Non-Profit': 'dodgerblue',
-            'For-Profit': 'rgb(127, 186, 0)',
-            'Individual': 'orange',
-            'Funding': '#FF7460',
-            'Data': '#84C2FF',
-            'Employment': '#EE73FF',
-            'Collaboration': '#FFD955'
+            'Government': {'focused' : "rgba(242, 80, 34, 1)", "unfocused" : "rgba(242, 80, 34, 0.1)"},
+            'Non-Profit': {'focused' : "rgba(30, 144, 255, 1)", "unfocused" : "rgba(30, 144, 255, 0.1)"},
+            'For-Profit': {'focused' : "rgba(127, 186, 0, 1)", "unfocused" : "rgba(127, 186, 0, 0.1)"},
+            'Individual': {'focused' : "rgba(255, 175, 44, 1)", "unfocused" : "rgba(255, 175, 44, 0.1)"},
+            'Funding': {'focused' : '#FF7460', "unfocused" : '#E3DFE4'},
+            'Data': {'focused' : '#84C2FF', "unfocused" : '#E3DFE4'},
+            'Employment': {'focused' : '#EE73FF', "unfocused" : '#E3DFE4'},
+            'Collaboration': {'focused' : '#FFD955', "unfocused" : '#E3DFE4'}
         };
 
         var offsets = {
@@ -409,6 +409,10 @@ angular.module('civic-graph', ['ui.bootstrap', 'leaflet-directive'])
         $("#canvas-force").click(function (e) {
             var oX = e.offsetX,
                 oY = e.offsetY;
+            $scope.showLicense = false;
+            $scope.clickedEntity.entity = null;
+            console.log($scope.showLicense)
+            var entityFound = false;
                 data.nodes.forEach(function(d) {
                     if ($scope.sizeBy == "employees") {
                         var k = ((d.employees/200000) * 5 )+1.5;
@@ -416,40 +420,64 @@ angular.module('civic-graph', ['ui.bootstrap', 'leaflet-directive'])
                         var k = ((d.followers/11000000) * 5 )+1.5;;
                     }
                     if(isInsideCircle(oX, oY, d.x+offsets[d.type][0], d.y+offsets[d.type][1], 4.5*k)) {
-
+                        $scope.currentEntity = d;
+                        console.log(d.name)
+                        entityFound = true;
+                        $scope.setEntity(d);
+                        $scope.clickedEntity.entity = d;
+                        focus(d);
                     }
                 })
+            if (!entityFound) {
+                $scope.currentEntity = null;
+            }
+                tick();
+            $scope.actions.interacted = true
+            $scope.safeApply();
         })
 
         var context = canvas.node().getContext("2d");
         var tick = function() {
+            var showEntities = [];
+
             context.clearRect(0, 0, width, height);
 
             // draw links
             context.strokeStyle = "#ccc";
             _.forEach($scope.connections, function(connections, type) {
                 connections.forEach(function(d) {
-                    if ($scope.connectionTypes[type] && ($scope.entityTypes[d.target.type] && $scope.entityTypes[d.source.type])) {    
+                    if ($scope.connectionTypes[type] && ($scope.entityTypes[d.target.type] && $scope.entityTypes[d.source.type]) && d.source == $scope.currentEntity || d.target == $scope.currentEntity || !$scope.currentEntity) {
                         context.beginPath()
                         context.moveTo(d.source.x+offsets[d.source.type][0], d.source.y+offsets[d.source.type][1]);
                         context.lineTo(d.target.x+offsets[d.target.type][0], d.target.y+offsets[d.target.type][1]); 
-                        context.strokeStyle = colors[type]
+                        context.strokeStyle = colors[type]['focused']
                         context.stroke()
                         context.closePath();
+                        showEntities.push(d.source);
+                        showEntities.push(d.target);
+
                     }
                 });
             });
+            // console.log(showEntities);
             nodes = [];
             var entityNames = [];
             data.nodes.forEach(function(d) {
                 if ($scope.entityTypes[d.type]) {
+                    // if ($scope.currentEntity){ debugger;};
+                    var focus;
+                    if (d === $scope.currentEntity || !$scope.currentEntity || showEntities.indexOf(d) >= 0){
+                        focus = "focused";
+                     } else {
+                        focus = "unfocused";
+                     }
                     if ($scope.sizeBy == "employees") {
                     var k = ((d.employees/200000) * 5 )+1.5;
                     } else {
                     var k = ((d.followers/11000000) * 5 )+1.5;;
                     }
                     context.beginPath();
-                    context.fillStyle = colors[d.type];
+                    context.fillStyle = colors[d.type][focus];
                     context.arc(d.x+offsets[d.type][0], d.y+offsets[d.type][1], 4.5*k, 0, 2 * Math.PI);
                     nodes.push([d.name,d.name, d.x+offsets[d.type][0], d.y+offsets[d.type][1], 4.5*k, 0, 2 * Math.PI])
                     if (d.wellconnected) {
@@ -472,7 +500,7 @@ angular.module('civic-graph', ['ui.bootstrap', 'leaflet-directive'])
             .nodes(data.nodes)
             .links(data.links)
             .on("tick", tick)
-            .charge(-5)
+            .charge(-2)
             .linkStrength(0.1)
             .linkDistance(50)
             .start();
