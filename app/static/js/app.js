e3073887ea20ac83306c67990199b00deb77e9cb
David Lorente <david.l.castilla@gmail.com>
2015-07-01 17:45:12 -0400

    Update app.js

--- app/static/js/app.js
+++ app/static/js/app.js
@@ -397,8 +397,8 @@ angular.module('civic-graph', ['ui.bootstrap', 'leaflet-directive'])
             height = $("#canvas-force").height();
 
         var canvas = d3.select("div#canvas-force").append("canvas")
-            .attr("width", width*window.devicePixelRatio)
-            .attr("height", height*window.devicePixelRatio);
+            .attr("width", width)
+            .attr("height", height);
 
         var isInsideCircle = function (x, y, cx, cy, radius) {
             var dx = x-cx
