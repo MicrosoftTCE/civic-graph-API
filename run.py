5bb34b74e4c7b4ba4004f37019450d08f6f22781
David Lorente <david.l.castilla@gmail.com>
2015-07-22 13:33:16 -0400

    Update run.py

--- run.py
+++ run.py
@@ -1,8 +1,6 @@
 #!/usr/bin/env python
 
 from app import app
-from flask.ext.cors import CORS
-cors = CORS(app)
 
 if __name__=="__main__":
-	app.run(host='0.0.0.0', debug=True)
+	app.run(debug=True)
