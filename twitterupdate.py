4884628005cf50a9294976233c3a3d48848d51a4
David Lorente <david.l.castilla@gmail.com>
2015-08-18 16:37:40 -0400

    Update twitterupdate.py

--- twitterupdate.py
+++ twitterupdate.py
@@ -7,21 +7,21 @@ from secrets import consumer_key, consumer_secret, access_token, access_token_se
 entities = Entity.query.filter(Entity.twitter_handle!=None).all()
 
 api = twitter.Api(consumer_key=consumer_key,
-					consumer_secret=consumer_secret,
-					access_token_key=access_token,
-					access_token_secret=access_token_secret)
+                    consumer_secret=consumer_secret,
+                    access_token_key=access_token,
+                    access_token_secret=access_token_secret)
 
 for entity in entities:
     twitter_handle = entity.twitter_handle.strip('@ ')
     if len(twitter_handle) > 0:
         try:
             time.sleep(3)
-        	user = api.GetUser(screen_name=twitter_handle)
+            user = api.GetUser(screen_name=twitter_handle)
             followers = user.followers_count
             if followers:
-            	print entity.name
-            	print followers
-            	entity.followers = followers
+                print entity.name
+                print followers
+                entity.followers = followers
         except:
             print 'ERROR', entity.name
 
