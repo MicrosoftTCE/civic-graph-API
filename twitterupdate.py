bb98d4081ccc296dc7b2b0e8cb00aa532064d0e8
David Lorente <david.l.castilla@gmail.com>
2015-08-18 16:02:23 -0400

    Create twitterupdate.py

---
+++ twitterupdate.py
@@ -0,0 +1,26 @@
+import time
+import json
+import twitter
+from app.models import Entity
+from database import db
+from secrets import consumer_key, consumer_secret, access_token, access_token_secret
+
+entities = Entity.query.filter(Entity.twitter_handle!=None).all()
+
+api = twitter.Api(consumer_key=consumer_key,
+					consumer_secret=consumer_secret,
+					access_token_key=access_token,
+					access_token_secret=access_token_secret)
+
+for entity in entities:
+    twitter_handle = entity.twitter_handle.strip('@ ')
+    if len(twitter_handle) > 0:
+        time.sleep(3)
+    	user = api.GetUser(screen_name=twitter_handle)
+        followers = user.followers_count
+        if followers:
+        	print entity.name
+        	print followers
+        	entity.followers = followers
+
+db.commit()
