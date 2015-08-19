f03214fac5c8beb0c438ffb9a168eec23b3e8777
David Lorente <david.l.castilla@gmail.com>
2015-08-19 10:07:16 -0400

    Update twitterupdate.py

--- twitterupdate.py
+++ twitterupdate.py
@@ -15,7 +15,7 @@ for entity in entities:
     twitter_handle = entity.twitter_handle.strip('@ ')
     if len(twitter_handle) > 0:
         try:
-            time.sleep(3)
+            time.sleep(6)
             user = api.GetUser(screen_name=twitter_handle)
             followers = user.followers_count
             if followers:
