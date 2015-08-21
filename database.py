faa0b3d7dbb4624ea8f87a5eadba7e0320732ef1
Rasmi <rrelasmar@gmail.com>
2015-08-21 13:59:05 +0000

    Trying to fix mysql errors.

--- database.py
+++ database.py
@@ -3,7 +3,7 @@ from sqlalchemy.orm import scoped_session, sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 from config import SQLALCHEMY_DATABASE_URI
 
-engine = create_engine(SQLALCHEMY_DATABASE_URI, convert_unicode=True)
+engine = create_engine(SQLALCHEMY_DATABASE_URI, convert_unicode=True, pool_recycle=3600)
 db = scoped_session(sessionmaker(autocommit=False,
                                 autoflush=False,
                                 bind=engine))
