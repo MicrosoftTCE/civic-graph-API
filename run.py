55069943163e97467c69b1211fffdcd8b57e9908
Nihal Mirpuri <nihal.mirpuri@gmail.com>
2016-01-07 14:11:50 -0500

    cleaned up unused and deprecated files, moved flask secret key to secrets.py (REQUIRES GIT SQUASH)

--- run.py
+++ run.py
@@ -2,7 +2,8 @@
 
 from app import app
 from flask import Flask, session
+from secrets import flask_session_secret_key
 
 if __name__=="__main__":
-	app.secret_key = 'A\xfbf\xd0\x86\r\xd9\xc9\x04\x8f\xd4\x04\xe0.2^\x82\xc7)\xbf\x0e\x98f\xfc\xa2\x91J0be\xa0\xd6\x03\xd6\x17\xee,\xef\x93'
+	app.secret_key = flask_session_secret_key
 	app.run(debug=True)
