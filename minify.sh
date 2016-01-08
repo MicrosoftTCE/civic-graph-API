f8f975ea1a2b8e98266f04fdd8def8f631dcbfe2
Nihal Mirpuri <nihal.mirpuri@gmail.com>
2016-01-08 12:35:19 -0500

    updated minify.sh to cd back to root dir

--- minify.sh
+++ minify.sh
@@ -1,8 +1,8 @@
-civicenv
-cd ~/civic-graph/app/static/js
+cd /home/civic/civic-graph/app/static/js
 rm app.min.js
 python -m jsmin app.js > app.min.js
-cd ~/civic-graph/app/static/css
+cd /home/civic/civic-graph/app/static/css
 rm app.min.css
 python -m csscompressor app.css -o app.min.css
 echo 'Minified.'
+cd ../../..
