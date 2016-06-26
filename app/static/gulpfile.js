d146f19ced470efa51d22b3c55f2d30048fa9549
Ethan Garrison <egarrison@varickmm.com>
2016-06-26 15:48:03 -0400

    Add gulp integration in order to speed up compile time and better structure js files
    The original version had all js files in a single file, making it
    difficult to edit or find parts of the project.  Now the files are
    broken out and gulp is used to bring them back together and then
    minify.  This will be useful for any later integration of plugins as
    well.
    
    The checkin files which appear to be a seperate project entirely were
    moved into the parent directory in order to prevent them from being
    confused as part of this project.
    
    Signed-off-by: Ethan Garrison <egarrison@varickmm.com>

---
+++ app/static/gulpfile.js
@@ -0,0 +1,35 @@
+(function (require) {
+
+    'use strict';
+
+    var gulp   = require('gulp'),
+        uglify = require('gulp-uglify'),
+        cleanCss = require('gulp-clean-css'),
+        concat = require('gulp-concat'),
+        notify = require('gulp-notify'),
+        build = 'build/',
+        jsSrc = 'js/**/*.js',
+        minifiedFile = 'app.min.js',
+        concatConfig = {newLine: '\n;'},
+        cssSrc = 'css/**/*.css',
+        minifiedCss = 'app.min.css';
+
+    gulp.task('js', function() {
+        return gulp.src(jsSrc)
+            .pipe(uglify())
+            .on('error', notify.onError("Error: <%= error.message %>"))
+            .pipe(concat(minifiedFile), concatConfig)
+            .pipe(gulp.dest(build));
+    });
+
+    gulp.task('css', function() {
+        return gulp.src(cssSrc)
+            .pipe(concat(minifiedCss))
+            .pipe(cleanCss())
+            .pipe(gulp.dest(build));
+    });
+
+    gulp.task('default', ['js', 'css'], function () {
+
+    });
+})(require);
