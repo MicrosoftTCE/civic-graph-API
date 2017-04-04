2ee6b9a6be53490f56103672a3f2f6caf5a6cff6
Ross Dakin <rossdakin@gmail.com>
2017-04-03 20:09:42 -0400

    Install Application Insights

--- app/main.py
+++ app/main.py
@@ -4,6 +4,9 @@ from flask_redis import FlaskRedis
 
 from database import db
 
+from applicationinsights.exceptions import enable
+from applicationinsights.requests import WSGIApplication
+
 redis_store = FlaskRedis()
 
 def create_app():
@@ -13,6 +16,12 @@ def create_app():
     return app
 
 app = create_app()
+
+if app.config['APP_INSIGHTS_INSTRUMENTATION_KEY']:
+    # log unhandled exceptions to Azure Application Insights
+    enable(app.config['APP_INSIGHTS_INSTRUMENTATION_KEY'])
+    # log requests to Azure Application Insights
+    app.wsgi_app = WSGIApplication(app.config['APP_INSIGHTS_INSTRUMENTATION_KEY'], app.wsgi_app)
 
 cache = Cache(app, config={
     'CACHE_TYPE': 'redis',
