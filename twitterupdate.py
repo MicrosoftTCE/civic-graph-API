20fd3ea89a0dbecd522cc0ebbc37459a81646814
Prathm Juneja <pjuneja@nd.edu>
2018-06-04 12:28:15 -0400

    This version of twitter fully updates the db...
    but you have to manually flush the redis cache by rebooting it in azure. The next two action items are to figure out how to clear the cache from this script, and then how to automate it (probably crontab?)

--- twitterupdate.py
+++ twitterupdate.py
@@ -3,14 +3,14 @@ import twitter
 from app.models import Entity
 from database import db
 from config import environ_get
+from app import cache
 
 entities = Entity.query.filter(Entity.twitter_handle!=None).all()
-
 api = twitter.Api(environ_get('consumer_key'),
                   environ_get('consumer_secret'),
                   environ_get('access_token_key'),
                   environ_get('access_token_secret'))
-
+x = 0
 for entity in entities:
     twitter_handle = entity.twitter_handle.strip('@')
     if len(twitter_handle) > 0:
@@ -19,10 +19,13 @@ for entity in entities:
             followers = user.followers_count
             description = user.description
             if followers:
+                #print entity.name, '; Actual: ', followers, '; Stored: ', entity.followers
                 entity.followers = followers
             if description:
                 entity.description = description
         except:
-            print 'ERROR', entity.name
-
+            print 'ERROR'
+    x = x+1
+    if (x % 100 == 0):
+        db.commit()
 db.commit()
